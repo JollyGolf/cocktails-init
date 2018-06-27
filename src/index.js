b9a814093f48bdb205ea6c4f9615e73cb09364c8
jollygolf <zhigamovky>
2018-06-27 14:10:26 +0300

    Installed ESlint and fixed all errors

--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
-import { getRandomCocktail } from './api'
-import _ from 'lodash'
+/* global document */
+import _ from 'lodash';
+import { getRandomCocktail } from './api';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root')
-  _.times(10).forEach(async (_, index) => {
-    const { body: { drinks } } = await getRandomCocktail()
-    drinks.forEach(({ strDrink, strDrinkThumb, strInstructions, ...rest}) => {
+  const root = document.getElementById('root');
+  _.times(10).forEach(async (index) => {
+    const { body: { drinks } } = await getRandomCocktail();
+    drinks.forEach(({
+      strDrink, strDrinkThumb, strInstructions, ...rest
+    }) => {
       function setAttributes(el, attrs) {
-        Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]))
+        Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]));
       }
-      const cocktailField = document.createElement('div')
-      setAttributes(cocktailField, { class: 'cocktail root__cocktail cocktail_item_' + index})
-      root.appendChild(cocktailField)
+      const cocktailField = document.createElement('div');
+      setAttributes(cocktailField, { class: `cocktail root__cocktail cocktail_item_${index}` });
+      root.appendChild(cocktailField);
 
-      const titleField = document.createElement('div')
-      titleField.appendChild(document.createTextNode('title: ' + strDrink))
-      cocktailField.appendChild(titleField)
+      const titleField = document.createElement('div');
+      titleField.appendChild(document.createTextNode(`title: ${strDrink}`));
+      cocktailField.appendChild(titleField);
 
-      const imageField = document.createElement('img')
-      setAttributes(imageField, { src: strDrinkThumb, class: 'image cocktail__image'})
-      cocktailField.appendChild(imageField)
+      const imageField = document.createElement('img');
+      setAttributes(imageField, { src: strDrinkThumb, class: 'image cocktail__image' });
+      cocktailField.appendChild(imageField);
 
-      const instructionField = document.createElement('div')
-      instructionField.appendChild(document.createTextNode('Instruction: ' + strInstructions))
-      cocktailField.appendChild(instructionField)
+      const instructionField = document.createElement('div');
+      instructionField.appendChild(document.createTextNode(`Instruction: ${strInstructions}`));
+      cocktailField.appendChild(instructionField);
 
-      const ingredientsField = document.createElement('div')
-      setAttributes(ingredientsField, {class: 'cocktail__ingridients ingridient_item_' + index})
-      ingredientsField.appendChild(document.createTextNode('Ingridients: '))
-      cocktailField.appendChild(ingredientsField)
-      const hr = document.createElement('hr')
+      const ingredientsField = document.createElement('div');
+      setAttributes(ingredientsField, { class: `cocktail__ingridients ingridient_item_${index}` });
+      ingredientsField.appendChild(document.createTextNode('Ingridients: '));
+      cocktailField.appendChild(ingredientsField);
+      const hr = document.createElement('hr');
 
       Object.keys(rest)
-        .filter((value) => value.match(/strIngredient\d+/))
+        .filter(value => value.match(/strIngredient\d+/))
         .forEach((key) => {
-        if(!!rest[key]) {
-          ingredientsField.appendChild(document.createTextNode(rest[key] + ', '))
-          root.appendChild(hr)
-        } 
-      })
-      const allIngredient = document.createTextNode(document.getElementsByClassName('ingridient_item_' + index)[0].textContent.slice(0, -2) + '.')
-      while (ingredientsField.firstChild) ingredientsField.removeChild(ingredientsField.firstChild)
-      ingredientsField.appendChild(allIngredient)
-    })
-  })
-})
+          if (rest[key]) {
+            ingredientsField.appendChild(document.createTextNode(`${rest[key]}, `));
+            root.appendChild(hr);
+          }
+        });
+      const allIngredient = document.createTextNode(`${document.getElementsByClassName(`ingridient_item_${index}`)[0].textContent.slice(0, -2)}.`);
+      while (ingredientsField.firstChild) ingredientsField.removeChild(ingredientsField.firstChild);
+      ingredientsField.appendChild(allIngredient);
+    });
+  });
+});
