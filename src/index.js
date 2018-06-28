a043c809c015d7ec39a1566715c6d69739f806d7
jollygolf <zhigamovky>
2018-06-28 21:33:52 +0300

    Added stylesheets

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import { getRandomCocktail } from './api';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
+  const mainField = document.getElementById('main_field');
   _.times(10).forEach(async (index) => {
     const { body: { drinks } } = await getRandomCocktail();
     drinks.forEach(({
@@ -13,38 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
         Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]));
       }
       const cocktailField = document.createElement('div');
-      setAttributes(cocktailField, { class: `cocktail root__cocktail cocktail_item_${index}` });
-      root.appendChild(cocktailField);
-
-      const titleField = document.createElement('div');
-      titleField.appendChild(document.createTextNode(`title: ${strDrink}`));
-      cocktailField.appendChild(titleField);
+      setAttributes(cocktailField, { class: `cocktail main_field__cocktail cocktail_flexbox cocktail_theme cocktail_border_size_s cocktail_item_${index}` });
+      mainField.appendChild(cocktailField);
 
       const imageField = document.createElement('img');
-      setAttributes(imageField, { src: strDrinkThumb, class: 'image cocktail__image' });
+      setAttributes(imageField, {src: strDrinkThumb, class: 'cocktail__image' });
       cocktailField.appendChild(imageField);
 
-      const instructionField = document.createElement('div');
-      instructionField.appendChild(document.createTextNode(`Instruction: ${strInstructions}`));
-      cocktailField.appendChild(instructionField);
+      const titleField = document.createElement('div');
+      setAttributes(titleField, { class: 'cocktail__title title_theme text' });
+      titleField.appendChild(document.createTextNode(strDrink));
+      cocktailField.appendChild(titleField);
 
       const ingredientsField = document.createElement('div');
-      setAttributes(ingredientsField, { class: `cocktail__ingridients ingridient_item_${index}` });
+      setAttributes(ingredientsField, { class: `cocktail__ingredients ingredient_item_${index} text` });
       ingredientsField.appendChild(document.createTextNode('Ingridients: '));
       cocktailField.appendChild(ingredientsField);
-      const hr = document.createElement('hr');
 
       Object.keys(rest)
         .filter(value => value.match(/strIngredient\d+/))
         .forEach((key) => {
           if (rest[key]) {
             ingredientsField.appendChild(document.createTextNode(`${rest[key]}, `));
-            root.appendChild(hr);
           }
         });
-      const allIngredient = document.createTextNode(`${document.getElementsByClassName(`ingridient_item_${index}`)[0].textContent.slice(0, -2)}.`);
+      const allIngredient = document.createTextNode(`${document.getElementsByClassName(`ingredient_item_${index}`)[0].textContent.slice(0, -2)}.`);
       while (ingredientsField.firstChild) ingredientsField.removeChild(ingredientsField.firstChild);
       ingredientsField.appendChild(allIngredient);
+      const instructionField = document.createElement('div');
+      instructionField.appendChild(document.createTextNode(`Instruction: ${strInstructions}`));
+      setAttributes(instructionField, { class: 'cocktail__instruction instruction_theme text' });
+      cocktailField.appendChild(instructionField);
     });
   });
 });
