c6839f7d46fd978bf3e86c0f0451121ceb3551f6
jollygolf <zhigamovky>
2018-06-30 18:04:22 +0300

    Corrected errors on 'BEM'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 import { getRandomCocktail } from './api';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const mainField = document.getElementById('main_field');
+  const mainField = document.getElementById('mainfield');
   _.times(10).forEach(async (index) => {
     const { body: { drinks } } = await getRandomCocktail();
     drinks.forEach(({
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         Object.keys(attrs).forEach(key => el.setAttribute(key, attrs[key]));
       }
       const cocktailField = document.createElement('div');
-      setAttributes(cocktailField, { class: `cocktail main_field__cocktail cocktail_flexbox cocktail_theme cocktail_border_size_s cocktail_item_${index}` });
+      setAttributes(cocktailField, { class: `cocktail mainfield__cocktail mainfield__cocktail-flexbox mainfield__cocktail-theme-transparency mainfield__cocktail-border-size-s cocktail-item-${index}` });
       mainField.appendChild(cocktailField);
 
       const imageField = document.createElement('img');
@@ -21,12 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
       cocktailField.appendChild(imageField);
 
       const titleField = document.createElement('div');
-      setAttributes(titleField, { class: 'cocktail__title title_theme text' });
+      setAttributes(titleField, { class: 'cocktail__title text' });
       titleField.appendChild(document.createTextNode(strDrink));
       cocktailField.appendChild(titleField);
 
       const ingredientsField = document.createElement('div');
-      setAttributes(ingredientsField, { class: `cocktail__ingredients ingredient_item_${index} text` });
+      setAttributes(ingredientsField, { class: `cocktail__ingredients ingredient-item-${index} text` });
       ingredientsField.appendChild(document.createTextNode('Ingridients: '));
       cocktailField.appendChild(ingredientsField);
 
@@ -37,12 +37,12 @@ document.addEventListener('DOMContentLoaded', () => {
             ingredientsField.appendChild(document.createTextNode(`${rest[key]}, `));
           }
         });
-      const allIngredient = document.createTextNode(`${document.getElementsByClassName(`ingredient_item_${index}`)[0].textContent.slice(0, -2)}.`);
+      const allIngredient = document.createTextNode(`${document.getElementsByClassName(`ingredient-item-${index}`)[0].textContent.slice(0, -2)}.`);
       while (ingredientsField.firstChild) ingredientsField.removeChild(ingredientsField.firstChild);
       ingredientsField.appendChild(allIngredient);
       const instructionField = document.createElement('div');
       instructionField.appendChild(document.createTextNode(`Instruction: ${strInstructions}`));
-      setAttributes(instructionField, { class: 'cocktail__instruction instruction_theme text' });
+      setAttributes(instructionField, { class: 'cocktail__instruction text' });
       cocktailField.appendChild(instructionField);
     });
   });
